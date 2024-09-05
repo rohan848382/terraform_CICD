5538ba2014abd560aa3224599b64483f7c0b8793
Rohan Rajendra Nikam <152033186+rohan848382@users.noreply.github.com>
2024-09-05 11:39:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "dev" {
   ami                    = "ami-0a4408457f9a03be3"
   instance_type          = "t2.micro"
   key_name               = "public"
-  subnet_id              = "subnet-0a82e85edc27004b2"
   
 }
 
