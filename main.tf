1381e3e1429ebea587ae751e364e936ce12235b1
Rohan Rajendra Nikam <152033186+rohan848382@users.noreply.github.com>
2024-09-06 11:36:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ resource "aws_instance" "dev" {
   ami                    = "ami-0a4408457f9a03be3"
   instance_type          = "t2.micro"
   key_name               = "public"
+
+tags = {
+Name = "Dev"
+}
   
 }
 
